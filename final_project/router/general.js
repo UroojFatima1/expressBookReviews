522db42c8f1134b61b335b542026afe5b83c665b
Urooj Fatima <uroojfatima1518@gmail.com>
2023-03-23 13:24:30 -0400

    till task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,24 +7,32 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  res.send(JSON.stringify({books},null,10));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  if(books[isbn]){
+  res.send(books[isbn]);}
+  else{
+  return res.status(300).json({message: "Yet to be implemented"})};
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
+  const author=req.params.author;
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
