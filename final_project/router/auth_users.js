f0cd754adc4844536041cdc22245a047a6d77210
Urooj Fatima <59086333+UroojFatima1@users.noreply.github.com>
2023-03-26 03:49:52 +0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,7 +7,14 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
-}
+let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }}
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
@@ -16,6 +23,9 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
+  if(isValid){
+      res.send("User is valid");
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
